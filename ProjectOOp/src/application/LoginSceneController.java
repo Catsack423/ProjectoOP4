6450a25d18a8c873cd3c34e7870cb3fdc14d52ae
Catsack423 <face11773@gmail.com>
2025-02-27 03:35:15 +0700

    ออกแบบหน้าcrate account
    กับคำสั่งปุ่มล็อคอินนิดหน่อยLogin

--- ProjectOOp/src/application/LoginSceneController.java
+++ ProjectOOp/src/application/LoginSceneController.java
@@ -1,10 +1,78 @@
 package application;
 
+
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class LoginSceneController {
-	 @FXML
-	 private AnchorPane AnchorLogin;
+    @FXML
+    private AnchorPane AnchorLogin;
 
+    @FXML
+    private Button CreateAccountButton;
+
+    @FXML
+    private PasswordField PassTextFiled;
+
+    @FXML
+    private TextField UsernameTextFiled;
+
+    @FXML
+    private Button loginButton;
+    
+    @FXML
+    private Label ErorLabel;
+    
+    
+    
+    public void LoginButtonClick(ActionEvent e) {
+    	
+    	String username = UsernameTextFiled.getText();
+    	String password= PassTextFiled.getText();
+    	
+    	if (username.isEmpty() || password.isEmpty()) { // ตรวจสอบว่าฟิลด์ว่างเปล่า
+	    	ErorLabel.setVisible(true);
+	    	ErorLabel.setText("Please enter username and password.");
+	    	delay2second(2);
+	        return;
+	    }else if(username.isEmpty() || password.isEmpty()) {
+    		ErorLabel.setVisible(true);
+    		ErorLabel.setText("Do not leave it emthy");
+    		delay2second(2);
+    		return;
+    	} 	
+    	
+    	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    void delay2second(int time) {
+    	Timer timer = new Timer();
+    	timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+            	ErorLabel.setVisible(false);
+                timer.cancel(); // ปิด Timer เมื่อทำงานเสร็จ
+            }
+        }, time*1000);
+    }
+    
+    
+    
+    
 }
--- ProjectOOp/src/application/Main.java
+++ ProjectOOp/src/application/Main.java
@@ -12,11 +12,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("LoginScene.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("LoginScene.fxml"));
+            Parent root = loader.load();
+			primaryStage.setScene(new Scene(root));
+            primaryStage.setTitle("Login Page");
+            primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
