fd0ec000913ff0b5f6b819dd261f179a60ed8617
Catsack423 <face11773@gmail.com>
2025-02-26 21:33:31 +0700

    initalize
    ตืดตั้ง

---
+++ ProjectOOp/src/SinginScene/SinginSceneController.java
@@ -0,0 +1,5 @@
+package SinginScene;
+
+public class SinginSceneController {
+
+}
---
+++ ProjectOOp/src/module-info.java
@@ -0,0 +1,9 @@
+module ProjectOOp {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
+	requires javafx.graphics;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	opens SinginScene to javafx.graphics, javafx.fxml;
+}
