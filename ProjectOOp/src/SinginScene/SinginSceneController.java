522ca38b652eb4f38d31503bb939ce02b257dd01
Catsack423 <face11773@gmail.com>
2025-02-27 23:37:40 +0700

    ทำการสร้างคำสั่งปุ่ม sign up
    ทำยังไม่เสร็จดีแต่ต่อกับData baseแล้ว

--- ProjectOOp/src/SinginScene/SinginSceneController.java
+++ ProjectOOp/src/SinginScene/SinginSceneController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.TextField;
 public class SinginSceneController {
 	@FXML
     private Button ArrowbackButton;
+
     @FXML
     private Button CreatAccountButton;
 
@@ -26,4 +27,35 @@ public class SinginSceneController {
     
     
     
+    
+    public void CreatAccount() {
+    	String username = UsernameTextFiled.getText();
+    	String password= PassTextFiled.getText();
+    	String repassword = RepasswordTextFiled.getText();
+    	
+    	if (username.isEmpty() && password.isEmpty()) { // ตรวจสอบว่าฟิลด์ว่างเปล่า
+	    	ErorLabel.setVisible(true);
+	    	ErorLabel.setText("Please enter username and password.");
+	    	
+	        return;
+	    }else if(username.isEmpty() || password.isEmpty()|| repassword.isEmpty()) {
+    		// กรณีช้่องว่าง
+	    	ErorLabel.setVisible(true);
+    		ErorLabel.setText("Do not leave it empty");
+  
+    		return;
+    	} 
+	    else if(password.length()< 8 ) { //รหัสน้อยกว่า8ตัว
+    		ErorLabel.setText("Password must be more 8 letters");
+    		ErorLabel.setVisible(true);
+    		return;
+    	}else if(password!=repassword) { //รหัสกับใส่รหัสไม่เหมือนกัน
+    		ErorLabel.setText("Please Enter same password");
+    		ErorLabel.setVisible(true);
+    		return;
+    	}
+    	
+    	
+    	
+	}
 }
