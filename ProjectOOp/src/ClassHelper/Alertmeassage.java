1bed4ef4213af55c950ccf45232c5fcbe852ea7b
Catsack423 <face11773@gmail.com>
2025-02-28 04:55:01 +0700

    Create aleart class

---
+++ ProjectOOp/src/ClassHelper/Alertmeassage.java
@@ -0,0 +1,29 @@
+package ClassHelper;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Alertmeassage {
+	Alert alert;
+	
+	public void errorMessage(String mess) {
+		alert = new Alert(AlertType.ERROR);
+		alert.setTitle("Error Message");
+		alert.setHeaderText(null);
+		alert.setContentText(mess);
+		alert.showAndWait();
+		
+		
+	}
+	
+	
+	public void succesMessage(String mess) {
+		alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("Information Message");
+		alert.setHeaderText(null);
+		alert.setContentText(mess);
+		alert.showAndWait();
+		
+		
+	}
+}
